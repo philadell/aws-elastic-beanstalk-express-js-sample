dd25616f84dcb1c32cd2eff7c09681913b477f96
Eckhardt Philipp <eckhardt.philipp@ericsson.com>
2021-04-15 15:03:25 +0200

    text change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hallo Welt!'));
+app.get('/', (req, res) => res.send('Hallo liebe Katja!\nWie ist es gestern gelaufen?\Hoffentlich sehr gut!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
